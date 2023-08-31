1e73a405ec104ef59ccdc117c9e22cfdcd405368
Admonia <dancingshadows309@gmail.com>
2023-08-31 18:03:08 -0400

    Anime nerdom begins, we have our users/posts start

---
+++ server/db/seedData.js
@@ -0,0 +1,20 @@
+//users
+
+const users = [
+    {username: 'Admonia', password: 'AnimeisLyfe'},
+    {username: 'Sonic-D12', password: 'TailsissuperSaiyen'},
+    {username: 'R2D2Silver', password: 'Padme'},
+    {username: 'DracoSeraider', password: 'blue21'},
+    {username: 'Marvolo', password: 'triten420'},
+    {username: 'Lala-Poe', password: 'writing-desk-Raven'},
+]
+
+//posts
+
+const posts = [
+    {title: 'Itachi VS Goku', body: 'Does anyone else think that although Goku is arguably the strongest anime character in existence, he would lose to Itachi Uchiha? Just looking into his eyes, which warriors do to show respect, will trap him in a mangekyou!'},
+    {title: '', body: ''},
+    {title: '', body: ''},
+    {title: '', body: ''},
+    {title: '', body: ''},
+]
