4de76e9a6d536f26c933bc418b5f2508ad11121e
Admonia <dancingshadows309@gmail.com>
2023-09-03 12:12:47 -0400

    ~hello world 8089

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-// GET /api/health
-router.get('/health', (req, res, next) => {
-    res.send('OK');
+// const express = require('express');
+const app = express();
+
+// Import and use your route files
+const usersRouter = require('./routes/users');
+app.use('/api/users', usersRouter);
+
+// Add more routes as needed
+
+const PORT = process.env.PORT || 8089;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+// // GET /api/health
+// router.get('/health', (req, res, next) => {
+//     res.send('OK');
+// });
+
+
 
 router.use('/users', require('./users'));
 
-router.use('/posts', require('./posts'));
+// // router.use('/posts', require('./posts'));
 
-router.use('/anime', require('./anime'));
+// // router.use('/anime', require('./anime'));
 
 module.exports = router;
---
+++ server/api/users.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db'); // Import the database connection
+
+// Define API routes
+router.get('/', async (req, res, next) => {
+  try {
+    // Query the database
+    const results = await db.query('SELECT * FROM users');
+    res.json(results.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Add more routes as needed (POST, PUT, DELETE, etc.)
+router.post('/', async (req, res, next) => {
+    try {
+      const { username, email } = req.body;
+      // Validate input data here if necessary
+  
+      // Insert a new user into the database
+      const result = await db.query(
+        'INSERT INTO users (username, email) VALUES ($1, $2) RETURNING *',
+        [username, email]
+      );
+  
+      res.status(201).json(result.rows[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
+  router.get('/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      // Query the database to get a user by ID
+      const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+  
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json(result.rows[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
+  router.put('/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { username, email } = req.body;
+      // Validate input data here if necessary
+  
+      // Update user information in the database
+      const result = await db.query(
+        'UPDATE users SET username = $1, email = $2 WHERE id = $3 RETURNING *',
+        [username, email, id]
+      );
+  
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json(result.rows[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
+  router.delete('/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      // Delete a user from the database by ID
+      const result = await db.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+  
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      next(error);
+    }
+  });
+        
+
+module.exports = router;
